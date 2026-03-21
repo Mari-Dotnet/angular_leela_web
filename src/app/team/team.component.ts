b48fd4468e342589e307291c9c62e03905ab2ddb
Mari Muthu <maripvm14@gmail.com>
2026-03-21 20:12:26 +0530

    DI

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, InjectionToken } from '@angular/core';
+import { MyService } from '../service/myservice.service';
+
+const myServiceToken=new InjectionToken<MyService>('service'); // creating token for service
 
 @Component({
   selector: 'app-team',
   standalone: true,
   imports: [],
   templateUrl: './team.component.html',
-  styleUrl: './team.component.css'
+  styleUrl: './team.component.css',
+  providers: [{
+    provide:  myServiceToken,
+    useExisting: MyService
+  }]
 })
 export class TeamComponent {
-
+constructor(private myService: MyService,
+  @Inject(myServiceToken) private myService2: MyService
+){ 
+  console.log(this.myService.getId());
+  console.log(this.myService2.getId());
 }
+  
+}
+
